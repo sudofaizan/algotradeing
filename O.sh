2f4917903253c24f41426a80b816ead007682f30
faizii.official <faizii.official@gmail.com>
2022-06-27 02:19:28 +0530

    loop 2 removed

--- O.sh
+++ O.sh
@@ -7,9 +7,9 @@ export amt=$(echo $(( $RANDOM % 10 + 6 )))
 export inc=0.80
 export aftr=$amt
 export i=$(echo $(( $RANDOM % 50 + 1 ))$(( $RANDOM % 50 + 1 ))$(( $RANDOM % 50 + 1 ))$(( $RANDOM % 50 + 1 ))$(( $RANDOM % 50 + 1 ))$(( $RANDOM % 50 + 1 ))$(( $RANDOM % 50 + 1 )))
-export a="EUR:USD_OTC"
+export a="EUR:USD"
 export t="1 MIN"
-for run in {1..28}; do
+for run in {1..29}; do
 n=$((n-1))
 export data="SERVER HAD RECIVIED ORDER FROM ML:ALGORITHM AND  PLACED THE TRADE ON USER: $i , APP=OLYMP TRADE , ASSET=$a , TIME=$t SUBSCRIPTION STATUS= $n / $p LEFT ! WAITING FOR RESULT OF TRADE FOR USER: $i  .want to Be a part of us? signup at http://www.Algotradeing.com "
 $(pwd)/xh "https://api.telegram.org/bot5234920052:AAEDxAVyUMHE5p_t1FwhC3HRUC9FycEz_Tc/sendMessage?chat_id=@algo_Tradeing&text=$data"
@@ -23,20 +23,5 @@ export data4="SERVER IS WAITNG FOR TRADE TO COMPLETE FOR USER $i "
 sleep 2
 $(pwd)/xh "https://api.telegram.org/bot5234920052:AAEDxAVyUMHE5p_t1FwhC3HRUC9FycEz_Tc/sendMessage?chat_id=@algo_Tradeing&text=$data4"
 
-done
-for run in {1..2}; do
-export arg="LOST"
-export data="@algo_tradeing SERVER HAD RECIVIED ORDER FROM ML:ALGORITHM AND  PLACED THE TRADE ON USER: $i , APP=OLYMP TRADE , ASSET=$a , TIME=$t SUBSCRIPTION STATUS= $n / $p LEFT ! WAITING FOR RESULT OF TRADE FOR USER: $i  .want to Be a part of us? signup at http://www.Algotradeing.com "
-$(pwd)/xh "https://api.telegram.org/bot5234920052:AAEDxAVyUMHE5p_t1FwhC3HRUC9FycEz_Tc/sendMessage?chat_id=@algo_Tradeing&parse_mode=markdown&text=$data"
-export data3="MACHINE LEARNING ALGORITHM IS LEARNING MARKET OF $a @algo_tradeing IS WAITING FOR ANALYSIS TO COMPLETE"
-$(pwd)/xh "https://api.telegram.org/bot5234920052:AAEDxAVyUMHE5p_t1FwhC3HRUC9FycEz_Tc/sendMessage?chat_id=@algo_Tradeing&parse_mode=markdownv2&text=||$data3||"
-sleep 59
-export aftr=$(echo $after 1 | awk '{print $1 }')
-export data2="RESULT FETCHED FROM SERVER FOR USER = $i TRADE= $arg (ASSEST=$a , TIME=$t , Trades left= $n / $p ) , USER_ID=$i , initial bal ${amt} USD current bal $aftr USD:   "
-$(pwd)/xh "https://api.telegram.org/bot5234920052:AAEDxAVyUMHE5p_t1FwhC3HRUC9FycEz_Tc/sendMessage?chat_id=@algo_Tradeing&parse_mode=markdown&text=$data2"
-export data4="SERVER IS WAITNG FOR TRADE TO COMPLETE FOR USER $i "
-sleep 2
-$(pwd)/xh "https://api.telegram.org/bot5234920052:AAEDxAVyUMHE5p_t1FwhC3HRUC9FycEz_Tc/sendMessage?chat_id=@algo_Tradeing&parse_mode=markdown&text=$data4"
-n=$((n-1))
-done
+
 done
--- bigO.sh
+++ bigO.sh
@@ -9,7 +9,7 @@ export aftr=$amt
 export i=$(echo $(( $RANDOM % 50 + 1 ))$(( $RANDOM % 50 + 1 ))$(( $RANDOM % 50 + 1 ))$(( $RANDOM % 50 + 1 ))$(( $RANDOM % 50 + 1 ))$(( $RANDOM % 50 + 1 ))$(( $RANDOM % 50 + 1 )))
 export a="EUR:USD"
 export t="1 MIN"
-for run in {1..28}; do
+for run in {1..29}; do
 n=$((n-1))
 export data3="MACHINE LEARNING ALGORITHM IS LEARNING MARKET OF $a AND WAITING FOR ANALYSIS TO BE COMPLETED"
 $(pwd)/xh "https://api.telegram.org/bot5234920052:AAEDxAVyUMHE5p_t1FwhC3HRUC9FycEz_Tc/sendMessage?chat_id=@algo_Tradeing&parse_mode=markdownv2&text=||$data3||"
@@ -26,20 +26,5 @@ $(pwd)/xh "https://api.telegram.org/bot5234920052:AAEDxAVyUMHE5p_t1FwhC3HRUC9Fyc
 
 
 done
-for run in {1..2}; do
-n=$((n-1))
-export data3="MACHINE LEARNING ALGORITHM IS LEARNING MARKET OF $a AND WAITING FOR ANALYSIS TO BE COMPLETED"
-$(pwd)/xh "https://api.telegram.org/bot5234920052:AAEDxAVyUMHE5p_t1FwhC3HRUC9FycEz_Tc/sendMessage?chat_id=@algo_Tradeing&parse_mode=markdownv2&text=||$data3||"
-sleep 1
-export data="SERVER HAD PLACED THE TRADE ON USER: $i , APP=OLYMP TRADE , ASSET=$a , TIME=$t SUBSCRIPTION STATUS= $n / $p LEFT ! WAITING FOR RESULT OF TRADE FOR USER: $i  .want to Automate your tradeing ? link your tradeing account at http://www.Algotradeing.com "
-$(pwd)/xh "https://api.telegram.org/bot5234920052:AAEDxAVyUMHE5p_t1FwhC3HRUC9FycEz_Tc/sendMessage?chat_id=@algo_Tradeing&text=$data"
-export data4="SERVER IS WAITNG FOR TRADE TO BE COMPLETED FOR USER $i "
-sleep 2
-$(pwd)/xh "https://api.telegram.org/bot5234920052:AAEDxAVyUMHE5p_t1FwhC3HRUC9FycEz_Tc/sendMessage?chat_id=@algo_Tradeing&text=$data4"
-sleep 59
-export aftr=$(echo $aftr $inc | awk '{print $1 }')
-export data2="RESULT FETCHED FROM SERVER FOR USER = $i TRADE= LOST ‼️ (ASSEST=$a , TIME=$t , Trades left= $n / $p ) , USER_ID=$i , initial bal ${amt} USD current bal $aftr USD  .  "
-$(pwd)/xh "https://api.telegram.org/bot5234920052:AAEDxAVyUMHE5p_t1FwhC3HRUC9FycEz_Tc/sendMessage?chat_id=@algo_Tradeing&text=$data2"
 
-done
 done
